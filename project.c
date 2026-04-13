0b5e71685b9f08c25188a73f380ff0d41b3a67fc
Hayden-E <hayeubi@outlook.com>
2026-04-13 14:51:05 -0400

    Added IF function

--- project.c
+++ project.c
@@ -88,7 +88,19 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
+    // Check that PC is a multiple of 4
+    if(PC % 4 != 0x0)
+        return 1;
 
+    // Make sure PC is in appropriate range
+    if(PC < 0x0000 || PC > 0xFFFF)
+        return 1;
+
+    // Fetch instruction from mem
+    *instruction = Mem[PC>>2];
+
+    // Success
+    return 0;
 }
 
 
@@ -96,16 +108,14 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-
+    
 }
-
-
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+    // Ella
 }
 
 /* Read Register */
@@ -122,14 +132,14 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    // Ella
 }
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-
+    // Ella
 }
 
 /* Read / Write Memory */
@@ -175,6 +185,6 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
-
+    // Ella or Tyler
 }
 
