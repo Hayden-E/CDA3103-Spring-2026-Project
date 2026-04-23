472be82728417a8ab2cac29a207056f37a2992ab
Donald Duck <145502747+astraAd@users.noreply.github.com>
2026-04-23 15:03:21 -0400

    ella code implimented and fix with haydens shitty function

--- project.c
+++ project.c
@@ -92,11 +92,7 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
     // Check that PC is a multiple of 4
     if(PC % 4 != 0x0)
         return 1;
-
-    // Make sure PC is in appropriate range
-    if(PC < 0x0000 || PC > 0xFFFF)
-        return 1;
-
+        
     // Fetch instruction from mem
     *instruction = Mem[PC>>2];
 
@@ -127,11 +123,86 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
     *jsec = instruction & (0x3FFFFFF);
 }
 
+    // Ella
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-    // Ella
+    // initialize
+    controls->RegDst = 0;
+    controls->Jump = 0;
+    controls->Branch = 0;
+    controls->MemRead = 0;
+    controls->MemtoReg = 0;
+    controls->ALUOp = 0;
+    controls->MemWrite = 0;
+    controls->ALUSrc = 0;
+    controls->RegWrite = 0;
+
+    switch(op)
+    {
+        case 0x0: // R type (add, sub, and, or, slt, sltu)
+        controls->RegDst = 1;
+        controls->ALUOp = 7; // 111 in binary (R type)
+        controls->RegWrite = 1;
+        break;
+
+        case 0x2: // jump
+        controls->RegDst = 2;
+        controls->Jump = 1;
+        controls->MemtoReg = 2;
+        controls->Branch = 2;
+        controls->ALUSrc = 2; 
+        break;
+
+        case 0x4: // beq
+        controls->RegDst = 2;
+        controls->Branch = 1;
+        controls->MemtoReg = 2;
+        controls->ALUOp = 1; // subtraction needed to determine if equal
+        break;
+
+        case 0x8: // addi
+        controls->RegWrite = 1;
+        controls->ALUSrc = 1;
+        break;
+
+        case 0xa: // slti
+        controls->ALUOp = 2;
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+        break;
+
+        case 0xb: // sltiu
+        controls->ALUOp = 3; // slt unsigned
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+        break;
+
+        case 0xf: // lw upper immediate
+        controls->ALUOp = 6; // need upper 16 bits so SHIFT!
+        controls->ALUSrc = 1;
+        controls->RegWrite = 1;
+        break;
+
+        case 0x23: // lw
+        controls->RegWrite = 1;
+        controls->MemtoReg = 1;
+        controls->MemRead = 1;
+        controls->ALUSrc = 1;
+        break;
+
+        case 0x2b: // sw
+        controls->RegDst = 2;
+        controls->MemtoReg = 2;
+        controls->MemWrite = 1;
+        controls->ALUSrc = 1;
+        break;
+
+        default: // halt
+            return 1;
+    }
+    return 0; // no halt
 }
 
 /* Read Register */
@@ -144,18 +215,80 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 }
 
 
+// Ella
 /* Sign Extend */
 /* 10 Points */
-void sign_extend(unsigned offset,unsigned *extended_value)
+void sign_extend(unsigned offset,unsigned *extended_value) 
 {
-    // Ella
+
+    // or...?
+    if((offset >> 15) == 1)
+    {
+        *extended_value = offset | 0xffff0000; // first 16 bits become 1's
+    }
+    else
+    {
+        *extended_value = offset & 0x0000ffff; // first 16 bits become 0's
+    }
 }
 
+// Ella
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-    // Ella
+    int ALUControl;
+
+    if(ALUOp < 0 || ALUOp > 7)
+    {
+        return 1; // halt b/c out of bounds
+    }
+
+    if(ALUOp == 7) // R-Type
+    {
+        switch(funct)
+        {
+            case 0x20: // add
+                ALUControl = 0;
+            break;
+            
+            case 0x22: // sub
+            ALUControl = 1;
+            break;
+
+            case 0x24: // and
+                ALUControl = 4;
+            break;
+
+            case 0x25: // or
+                ALUControl = 5;
+            break;
+
+            case 0x2a: // slt
+                ALUControl = 2;
+            break;
+
+            case 0x2b: // sltu
+                ALUControl = 3;
+            break;
+
+            default:
+                return 1; // halt
+        }
+        ALU(data1, data2, ALUControl, ALUresult, Zero);
+    }
+    else
+    {
+        if(ALUSrc == 1)
+        {
+            ALU(data1, extended_value, ALUOp, ALUresult, Zero);
+        }
+        else
+        {
+            ALU(data1, data2, ALUOp, ALUresult, Zero);
+        }
+    }
+    return 0;
 }
 
 /* Read / Write Memory */
